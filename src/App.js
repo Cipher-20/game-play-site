e10dc3c4b5c552e952571524afc3b3540035103f
Cipher-20 <kan20_2003@abv.bg>
2022-05-23 21:41:32 +0300

    Add basic routes

--- src/App.js
+++ src/App.js
@@ -8,20 +8,28 @@ import GameDetails from "./components/GameDetails"
 import GameCatalog from "./components/GameCatalog"
 
 function App() {
+  const router = {
+    '/home':WelcomeWorld,
+    '/login':Login,
+    '/register':Register,
+    '/create-game':CreateGame,
+    '/editGame':EditGame,
+    '/gameDetails':GameDetails,
+    '/games':GameCatalog,
+
+  }
   return (
-
     <div id="box">
-
     <Header />
 
       <main id="main-content">
       <WelcomeWorld />
-      <Login />
+      {/* <Login />
       <Register />
       <CreateGame />
       <EditGame />
       <GameDetails />
-      <GameCatalog />
+      <GameCatalog /> */}
 
       </main>
 
